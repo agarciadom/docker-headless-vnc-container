0c28e1513734647801da2504b3991bc210de1ca8
Antonio García-Domínguez <a.garcia-dominguez@york.ac.uk>
2023-12-06 15:29:17 +0000

    Update script with instructions

--- epsilon-build-and-run.sh
+++ epsilon-build-and-run.sh
@@ -4,4 +4,6 @@ docker build -t epsilon-vnc:latest -f Dockerfile.epsilon-xfce-vnc .
 
 docker run -d -p 6901:6901 epsilon-vnc:latest
 
-echo -e "Open http://localhost:6901/vnc.html in a browser\nPassword is: vncpassword" 
+echo "Open http://localhost:6901/vnc.html in a browser"
+echo "Password is: vncpassword"
+echo "Right-click on desktop, open a terminal, and run /headless/epsilon/eclipse"
